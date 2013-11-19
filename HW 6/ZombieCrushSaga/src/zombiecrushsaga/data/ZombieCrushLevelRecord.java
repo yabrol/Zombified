fae70ef529ed28611818d61558cd1467ad7505c3
yabrol <yukti.abrol@gmail.com>
2013-11-18 22:42:07 -0500

    changed saga screen graphics

---
+++ HW 6/ZombieCrushSaga/src/zombiecrushsaga/data/ZombieCrushLevelRecord.java
@@ -0,0 +1,19 @@
+package zombiecrushsaga.data;
+
+/**
+ * This class stores game results for a given level. Note that this is
+ * just a data holding class. It will be manipulated fully by the
+ * zombiecrushsagaRecord class, which stores all the records and manages
+ * loading and saving.
+ * 
+ * @author Yukti Abrol
+ */
+public class ZombieCrushLevelRecord
+{
+    public int gamesPlayed;
+    public int wins;
+    public int losses;
+    public long fastestTime;
+    public int highScore;
+    public int numStars;
+}
